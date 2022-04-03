83208707a0a632b2acd6d0cd6568f22954dbcd21
YC <saiyuki0804@gmail.com>
2022-04-03 16:38:14 +0800

    [YC] 新增appCode

--- ita.common/src/main/java/com/web/server/cnst/AppCode.java
+++ ita.common/src/main/java/com/web/server/cnst/AppCode.java
@@ -4,7 +4,7 @@ public class AppCode {
 
 	public static class SERVER {
 		/**
-		 * 成功
+		 * 增刪改查成功
 		 */
 		public enum SUCCESS implements IAppCode {
 			/**
@@ -52,6 +52,22 @@ public class AppCode {
 			 * 資料錯誤
 			 */
 			DATA_ERROR("ERROR_002"),
+			/**
+			 * 資料錯誤
+			 */
+			INSERT_ERROR("ERROR_003"),
+			/**
+			 * 資料錯誤
+			 */
+			QUERY_ERROR("ERROR_004"),
+			/**
+			 * 資料錯誤
+			 */
+			DELETE_ERROR("ERROR_005"),
+			/**
+			 * 資料錯誤
+			 */
+			UPDATE_ERROR("ERROR_006"),
 			;
 			
 			private String code;
@@ -101,17 +117,21 @@ public class AppCode {
 		 */
 		public enum AUTH implements IAppCode {
 			/**
+			 * 登入成功
+			 */
+			LOGIN_SUCCESS("AUTH_001"),
+			/**
 			 * 登入失敗
 			 */
-			LOGIN_FAIL("AUTH_001"),
+			LOGIN_FAIL("AUTH_002"),
 			/**
-			 * 尚未登入
+			 * 驗證錯誤
 			 */
-			NO_LOGIN("AUTH_002"),
+			NO_LOGIN("AUTH_003"),
 			/**
 			 * 無此功能權限
 			 */
-			NO_AUTH("AUTH_003"),;
+			NO_AUTH("AUTH_004"),;
 
 			private String code;
 
