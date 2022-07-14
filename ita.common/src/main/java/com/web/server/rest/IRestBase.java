4f6a0db700fac8b7849abaf6cfa2450921a7361f
YC <saiyuki0804@gmail.com>
2022-07-14 22:23:40 +0800

    [YC] 修正回傳前端內容格式架構

--- ita.common/src/main/java/com/web/server/rest/IRestBase.java
+++ ita.common/src/main/java/com/web/server/rest/IRestBase.java
@@ -5,16 +5,6 @@ public interface IRestBase {
 	/**
 	 * 產生統一的格式物件回傳至前端
 	 * @param appCode 後端的訊息代碼
-	 * @param appMsg 後端訊息
-	 * @param restData 回傳至前端的資料
-	 */
-	default RestResult buildResult(String appCode, String appMsg, Object restData) {
-		return new RestResult(appCode, appMsg, restData);
-	}
-	
-	/**
-	 * 產生統一的格式物件回傳至前端
-	 * @param appCode 後端的訊息代碼
 	 * @param restData 回傳至前端的資料
 	 */
 	default RestResult buildResult(String appCode, Object restData) {
