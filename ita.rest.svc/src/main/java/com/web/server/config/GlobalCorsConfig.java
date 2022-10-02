bdf42834b35d41e4c487f5af4ec402805521c221
YC <saiyuki0804@gmail.com>
2022-10-02 19:10:34 +0800

    [YC] 新版跨域設定

--- ita.rest.svc/src/main/java/com/web/server/config/GlobalCorsConfig.java
+++ ita.rest.svc/src/main/java/com/web/server/config/GlobalCorsConfig.java
@@ -1,14 +1,31 @@
 package com.web.server.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+
+@WebFilter(filterName="CorsFilter")
 @Configuration
-public class GlobalCorsConfig {
+public class GlobalCorsConfig implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+	}
 
 //	@Bean
 //	public CorsFilter corsFilter() {
