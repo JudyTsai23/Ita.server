d22b6035aeef7041f4e8c93bc1ddf00b3473a28f
YC <saiyuki0804@gmail.com>
2022-10-02 20:20:34 +0800

    [YC] 調整新版跨域設定

--- ita.rest.svc/src/main/java/com/web/server/config/GlobalCorsConfig.java
+++ ita.rest.svc/src/main/java/com/web/server/config/GlobalCorsConfig.java
@@ -19,11 +19,9 @@ public class GlobalCorsConfig implements Filter {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin","*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+		response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
 	}
 
